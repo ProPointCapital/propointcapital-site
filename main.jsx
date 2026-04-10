cb46d202f33be50392e351fdd3b21b27560e996c
ProPointCapital <info@propointcapital.com>
2026-04-10 16:59:49 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <defs>
+    <linearGradient id="g" x1="0" x2="1" y1="0" y2="1">
+      <stop offset="0%" stop-color="#7ff6d6"/>
+      <stop offset="50%" stop-color="#35d6ff"/>
+      <stop offset="100%" stop-color="#4de08f"/>
+    </linearGradient>
+  </defs>
+  <rect width="100" height="100" rx="24" fill="#07131a"/>
+  <path d="M28 76V24h24c15 0 24 8 24 21 0 14-10 22-25 22H42v9H28zm14-21h9c7 0 12-3 12-10 0-6-5-9-12-9h-9v19z" fill="url(#g)"/>
+</svg>
